f4ec08f37e69b42dec0952f5583fee6b27d583bf
matsba <ristimaki.matti@gmail.com>
2017-07-25 20:58:41 +0300

    Added datepicker and year selector to submit form

--- client/src/SubmitForm.js
+++ client/src/SubmitForm.js
@@ -1,31 +1,64 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import Select from 'react-select';
+import moment from 'moment';
+import _ from 'lodash';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-select/dist/react-select.css';
 
 const initialState = {
         title: '',
-        releaseDate: '',
+        releaseDate: moment(),
         originalReleaseYear: '',
         coverArtUrl: '',
         region: '',
     };
 
+
+const years =  _.range(1900, 2040).map((obj) => { 
+   var rObj = {};
+   rObj['label'] = obj;
+   rObj['value'] = obj;
+   return rObj;
+});
+
+const yearDefaulOption = [{
+    'label': 1970, 'value': 1970
+}]
 class SubmitForm extends Component {
 
     constructor(props) {
-    super(props);
-    this.state = initialState
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = initialState
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleYearChange = this.handleYearChange.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-    this.setState({
-      [name]: value
-    });
-  }
+        this.setState({
+        [name]: value
+        });
+    }
+
+    handleDateChange(date) {
+        this.setState({
+        releaseDate: date
+        });
+    }
+
+    handleYearChange(y) {
+        y = y.value
+        this.setState({
+        originalReleaseYear: y
+        });
+    }
 
   handleSubmit(event) {
     console.log(this.state)
@@ -39,6 +72,8 @@ class SubmitForm extends Component {
     this.setState(initialState)
     event.preventDefault();
   }
+
+
 
     render(){
 
@@ -54,11 +89,13 @@ class SubmitForm extends Component {
                         </div>
                         <label for="releaseDate" className="control-label col-sm-2">Release date</label>
                         <div className="col-sm-10">
-                            <input type="text" className="form-control" id="releaseDate" name="releaseDate" value={this.state.releaseDate} onChange={this.handleInputChange}/>
+                            <DatePicker className="form-control" id="releaseDate" name="releaseDate"
+                            dateFormat="YYYY/MM/DD" selected={this.state.releaseDate} onChange={this.handleDateChange}/>
                         </div>
                         <label for="originalReleaseYear" className="control-label col-sm-2">Original Release Year</label>
                         <div className="col-sm-10">
-                            <input type="text" className="form-control" id="originalReleaseYear" name="originalReleaseYear" value={this.state.originalReleaseYear} onChange={this.handleInputChange}/>
+                            <Select id="originalReleaseYear" name="originalReleaseYear" 
+                            value={this.state.originalReleaseYear} focusedOption={this.state.yearDefaulOption} onChange={this.handleYearChange} options={years} />
                         </div>
                         <label for="coverArtUrl" className="control-label col-sm-2">Cover Art</label>
                         <div className="col-sm-10">
